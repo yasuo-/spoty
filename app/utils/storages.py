ef9ecf52bbe9b031ce060fe3206ef98dc9b8cea6
yasuo kurihara <breitling1216@gmail.com>
2022-10-01 07:50:50 +0900

    change config file

--- app/utils/storages.py
+++ app/utils/storages.py
@@ -1,4 +1,5 @@
 from storages.backends.gcloud import GoogleCloudStorage
+from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticRootGoogleCloudStorage(GoogleCloudStorage):
@@ -7,5 +8,15 @@ class StaticRootGoogleCloudStorage(GoogleCloudStorage):
 
 
 class MediaRootGoogleCloudStorage(GoogleCloudStorage):
+    location = "media"
+    file_overwrite = False
+
+
+class StaticRootS3Boto3Storage(S3Boto3Storage):
+    location = "static"
+    default_acl = "public-read"
+
+
+class MediaRootS3Boto3Storage(S3Boto3Storage):
     location = "media"
     file_overwrite = False
